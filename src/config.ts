51462508b3c09e4a131b76964452e854e736a26f
Clément <clement.chazelas@etu.utc.fr>
2025-10-01 16:15:11 +0200

    Activation WallPic

---
+++ src/config.ts
@@ -0,0 +1,25 @@
+export const config = {
+    // UTCotiz config
+    UTCOTIZ_KEY: "utkey-IkF0oPtDkptECbLgRqzISYKN9y3U4WXNKwysDz6y2lq5naTRn41E5GwdtO",
+
+    // Weezevent config
+    WEEZEVENT_CLIENT_ID: '',
+    WEEZEVENT_CLIENT_SECRET: '',
+    WEEZEVENT_API_URL: "https://api.weezevent.com/pay/v2/",
+    ORGANIZATION_ID : 0,
+    EVENT_ID : 1,
+
+    // Pic config
+    API_URL : "https://pic.assos.utc.fr/api/",
+    WEBSOCKET_URL : "ws://localhost:9191/events",
+    EXONERATION_UIDS : ["4B52D3EC"],
+    JCAP_URL: "http://127.0.0.1:8080",
+
+    // Old config
+    WEEZEVENT_APP_KEY: '31f5809f43b3161dd8aefb7a79a5fc55',
+    WEEZEVENT_SYSTEM_ID: 'payutc',
+    WEEZEVENT_URL : "https://api.nemopay.net/services",
+    FUND_ID : 2,
+
+    BOURSE_EVENT: true,
+};
